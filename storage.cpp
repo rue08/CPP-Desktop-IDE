7eed3c9aa0b92f3d0fb5b1cf3f0ab897fadca135
Mehul Sharma <mehssi2004@gmail.com>
2026-04-16 17:51:59 +0530

    Improve cloud upload and download handling

--- storage.cpp
+++ storage.cpp
@@ -17,98 +17,75 @@ void Storage::setIdToken(const QString &idToken)
     m_idToken = idToken;
 }
 
-void Storage::setLocalFilePath(const QString &localFilePath)
+
+void Storage::setPendingTasks(const int& pendingTasks)
 {
-    m_localFilePath = localFilePath;
+    m_pendingTasks = pendingTasks;
 }
 
-void Storage::networkReplyReadyReadResponse()
+
+// void Storage::setCloudFileData(const QByteArray& fileData, const QString& filePath)
+// {
+//     QString remotePath = QString("users/%1/%2").arg(m_uid, QFileInfo(filePath).fileName());
+//     QString encodedPath = QUrl::toPercentEncoding(remotePath);
+//     QString uploadFileEndpoint = "https://firebasestorage.googleapis.com/v0/b/" + m_firebaseBucket + "/o?uploadType=media&name=" + encodedPath;
+
+//     newRequest = QNetworkRequest{QUrl(uploadFileEndpoint)};
+//     newRequest.setRawHeader("Authorization", ("Bearer " + m_idToken).toUtf8());
+//     newRequest.setHeader(QNetworkRequest::ContentTypeHeader, QString("application/octet-stream"));
+
+//     QNetworkReply* reply = m_networkAccessManager -> post(newRequest, fileData);
+
+//     connect(reply, &QNetworkReply::finished, this, [this, filePath]() {
+//         // This code runs ONLY when the reply is finished
+//         this -> networkReplyReadyRead(filePath);
+//     });
+// }
+
+
+void Storage::uploadFile(const QByteArray& fileData, const QString& localFilePath)
 {
-    QJsonDocument jsonDocument = QJsonDocument::fromJson(m_networkReply -> readAll());
-    writingFileMetadata(jsonDocument.object().value("name").toString());
-}
-
-void Storage::networkReplyReadyReadFiles()
-{
-    parseResponse(m_networkReply -> readAll());
-}
-
-void Storage::networkReplyDownloadFiles()
-{
-    QByteArray response = m_networkReply -> readAll();
-    emit setDownloadFile(response);
-}
-
-void Storage::listFiles()
-{
-    QString listFilesEndpoint = "https://firestore.googleapis.com/v1/projects/mehul-s-ide/databases/(default)/documents/Users/" + m_uid + "/Files";
-    newRequest = QNetworkRequest{QUrl(listFilesEndpoint)};
-    newRequest.setRawHeader("Authorization", ("Bearer " + m_idToken).toUtf8());
-    newRequest.setHeader(QNetworkRequest::ContentTypeHeader, QString("application/json"));
-    m_networkReply = m_networkAccessManager -> get(newRequest);
-
-    connect(m_networkReply, &QNetworkReply::readyRead, this, &Storage::networkReplyReadyReadFiles);
-}
-
-void Storage::downloadFile(const QString &cloudFilePath)
-{
-    QString downloadFileEndpoint = "https://firebasestorage.googleapis.com/v0/b/" + m_firebaseBucket + "/o/" + QString(QUrl::toPercentEncoding(cloudFilePath)) + "?alt=media";
-    newRequest = QNetworkRequest{QUrl(downloadFileEndpoint)};
-    newRequest.setRawHeader("Authorization", ("Bearer " + m_idToken).toUtf8());
-    m_networkReply = m_networkAccessManager -> get(newRequest);
-
-    connect(m_networkReply, &QNetworkReply::readyRead, this, &Storage::networkReplyDownloadFiles);
-}
-
-void Storage::parseResponse(const QByteArray &response)
-{
-    QJsonDocument jsonDocument = QJsonDocument::fromJson(response);
-
-    QJsonArray documents = jsonDocument.object().value("documents").toArray();
-    QByteArray doc = jsonDocument.object().value("documents").toJson();
-    jsonDocument = QJsonDocument::fromJson(doc);
-    QByteArray fields = jsonDocument.object().value("fields").toJson();
-    jsonDocument = QJsonDocument::fromJson(fields);
-    QString fileName = jsonDocument.object().value("name").toString();
-
-    for (int i = 0; i < documents.size(); i++)
-    {
-        QJsonObject fileMetaData = documents[i].toObject();
-        QJsonObject fields = fileMetaData["fields"].toObject();
-        QString fileName = fields["name"].toObject().value("stringValue").toString();
-        QString cloudFilePath = fields["cloudPath"].toObject().value("stringValue").toString();
-        emit setCloudFiles(fileName, cloudFilePath);
-    }
-}
-
-void Storage::uploadFile()
-{
-    QFile file(m_localFilePath);
-    file.open(QIODevice::ReadOnly);
-    QByteArray fileData = file.readAll();
-    file.close();
-
-    QString remotePath = QString("users/%1/%2").arg(m_uid, QFileInfo(m_localFilePath).fileName());
-
-    QString uploadFileEndpoint = "https://firebasestorage.googleapis.com/v0/b/" + m_firebaseBucket + "/o?uploadType=media&name=" + remotePath;
+    QString remotePath = QString("users/%1/%2").arg(m_uid, QFileInfo(localFilePath).fileName());
+    QString encodedPath = QUrl::toPercentEncoding(remotePath);
+    QString uploadFileEndpoint = "https://firebasestorage.googleapis.com/v0/b/" + m_firebaseBucket + "/o?uploadType=media&name=" + encodedPath;
 
     newRequest = QNetworkRequest{QUrl(uploadFileEndpoint)};
     newRequest.setRawHeader("Authorization", ("Bearer " + m_idToken).toUtf8());
     newRequest.setHeader(QNetworkRequest::ContentTypeHeader, QString("application/octet-stream"));
 
-    m_networkReply = m_networkAccessManager -> post(newRequest, fileData);
+    QNetworkReply* reply = m_networkAccessManager -> post(newRequest, fileData);
 
-    connect(m_networkReply, &QNetworkReply::readyRead, this, &Storage::networkReplyReadyReadResponse);
+    connect(reply, &QNetworkReply::finished, this, [this, localFilePath]() {
+        // This code runs ONLY when the reply is finished
+        this -> networkReplyReadyRead(localFilePath);
+    });
 }
 
 
-void Storage::writingFileMetadata(const QString &cloudFilePath)
+void Storage::networkReplyReadyRead(const QString& localFilePath)
 {
-    QString fileMetadataEndpoint = "https://firestore.googleapis.com/v1/projects/mehul-s-ide/databases/(default)/documents/Users/" + m_uid + "/Files";
+    QNetworkReply *reply = qobject_cast<QNetworkReply*>(sender());
+
+    QByteArray response = reply -> readAll();
+    QJsonDocument jsonDocument = QJsonDocument::fromJson(response);
+    QJsonObject jsonObject = jsonDocument.object();
+
+    // Firebase returns the full path in the "name" field on success
+    QString cloudFilePath = jsonObject.value("name").toString();
+
+    writeFirestoreMetadata(cloudFilePath, localFilePath);
+
+    reply->deleteLater();
+}
+
+
+void Storage::writeFirestoreMetadata(const QString &cloudFilePath, const QString& localFilePath)
+{
+    QString fileMetadataEndpoint = "https://firestore.googleapis.com/v1/projects/mehul-s-ide/databases/(default)/documents/Users/" + m_uid + "/Files/" + QFileInfo(localFilePath).fileName();
 
     QJsonObject fields;
     fields["name"] = QJsonObject {
-        {"stringValue", QFileInfo(m_localFilePath).fileName()}
+        {"stringValue", QFileInfo(localFilePath).fileName()}
     };
     fields["cloudPath"] = QJsonObject {
         {"stringValue", cloudFilePath}
@@ -123,5 +100,78 @@ void Storage::writingFileMetadata(const QString &cloudFilePath)
     newRequest.setRawHeader("Authorization", ("Bearer " + m_idToken).toUtf8());
     newRequest.setHeader(QNetworkRequest::ContentTypeHeader, QString("application/json"));
 
-    m_networkReply = m_networkAccessManager -> post(newRequest, jsonPayload.toJson());
+    QNetworkReply* reply = m_networkAccessManager -> sendCustomRequest(newRequest, "PATCH", jsonPayload.toJson());
+
+    connect(reply, &QNetworkReply::finished, this, [this, reply]() {
+        m_pendingTasks--;
+
+        // ONLY list files when the last one is done
+        if (m_pendingTasks <= 0)
+            this -> listFiles();
+
+        reply->deleteLater();
+    });
+}
+
+
+void Storage::listFiles()
+{
+    QString listFilesEndpoint = "https://firestore.googleapis.com/v1/projects/mehul-s-ide/databases/(default)/documents/Users/" + m_uid + "/Files";
+    QNetworkRequest request{QUrl(listFilesEndpoint)};
+    request.setRawHeader("Authorization", ("Bearer " + m_idToken).toUtf8());
+    request.setHeader(QNetworkRequest::ContentTypeHeader, QString("application/json"));
+
+    QNetworkReply *reply = m_networkAccessManager -> get(request);
+    connect(reply, &QNetworkReply::finished, this, &Storage::networkReplyFile);
+}
+
+
+void Storage::networkReplyFile()
+{
+    QNetworkReply *reply = qobject_cast<QNetworkReply*>(sender());
+
+    QByteArray response = reply -> readAll();
+    parseResponse(response);
+
+    reply->deleteLater();
+}
+
+
+void Storage::parseResponse(const QByteArray &response)
+{
+    QJsonDocument jsonDocument = QJsonDocument::fromJson(response);
+
+    QJsonArray documents = jsonDocument.object().value("documents").toArray();
+
+    for (int i = 0; i < documents.size(); i++)
+    {
+        QJsonObject fileMetaData = documents[i].toObject();
+        QJsonObject fields = fileMetaData.value("fields").toObject();
+
+        QString fileName = fields.value("name").toObject().value("stringValue").toString();
+        QString cloudFilePath = fields.value("cloudPath").toObject().value("stringValue").toString();
+
+        emit setCloudFiles(fileName, cloudFilePath);
+    }
+}
+
+
+void Storage::downloadFile(const QString &cloudFilePath)
+{
+    QString downloadFileEndpoint = "https://firebasestorage.googleapis.com/v0/b/" + m_firebaseBucket + "/o/" + QString(QUrl::toPercentEncoding(cloudFilePath)) + "?alt=media";
+    newRequest = QNetworkRequest{QUrl(downloadFileEndpoint)};
+    newRequest.setRawHeader("Authorization", ("Bearer " + m_idToken).toUtf8());
+
+    QNetworkReply* reply = m_networkAccessManager -> get(newRequest);
+
+    connect(reply, &QNetworkReply::readyRead, this, &Storage::networkReplyDownloadFile);
+}
+
+
+void Storage::networkReplyDownloadFile()
+{
+    QNetworkReply* reply = qobject_cast<QNetworkReply*>(sender());
+    QByteArray response = reply -> readAll();
+
+    emit setDownloadFile(response);
 }
--- storage.h
+++ storage.h
@@ -21,28 +21,34 @@ public:
 
     void setUid(const QString &uid);
     void setIdToken(const QString &idToken);
-    void setLocalFilePath(const QString &localFilePath);
-    void uploadFile();
     void downloadFile(const QString &cloudFilePath);
     void listFiles();
 
+
+    void setPendingTasks(const int &pendingTasks);
+
+    void uploadFile(const QByteArray &fileData, const QString &localFilePath);
 public slots:
-    void networkReplyReadyReadResponse();
-    void networkReplyReadyReadFiles();
-    void networkReplyDownloadFiles();
+    void networkReplyFile();
+    void networkReplyDownloadFile();
 
 private:
     QNetworkAccessManager *m_networkAccessManager;
-    QNetworkReply *m_networkReply;
     QNetworkRequest newRequest;
-    QString m_localFilePath;
     QString m_uid;
     QString m_idToken;
     QString m_firebaseBucket = "mehul-s-ide.firebasestorage.app";
 
+    QString cloudFileName = "";
 
     void parseResponse(const QByteArray &response);
-    void writingFileMetadata(const QString &cloudFilePath);
+
+    void writeFirestoreMetadata(const QString &cloudFilePath, const QString &localFilePath);
+
+    int m_pendingTasks;
+
+private slots:
+    void networkReplyReadyRead(const QString &localFilePath);
 
 signals:
     void setCloudFiles(const QString &fileName, const QString &cloudFilePath);
